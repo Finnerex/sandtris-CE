fb13fe93a4f4cd2e4b4e882ae199a7e8562c5260
Finnerex <61033009+Finnerex@users.noreply.github.com>
2024-01-05 20:45:39 -0500

    code cleaned up a bit

--- src/game_board.c
+++ src/game_board.c
@@ -1,12 +1,9 @@
-#include <string.h>
-#include <debug.h>
 #include "game_board.h"
 
 #define can_move_side(fd) (y > 0 && (x + fd >= 0 && x + fd < GAME_WIDTH))
 
 // bouta be slow as hell
 void update_columns(pixel_t pixels[GAME_WIDTH][GAME_HEIGHT]) {
-    // memcpy(old_pixels, pixels, sizeof (pixel_t) * 56 * 118);
     static int fall_direction = 1; // 1 for right, -1 for left
 
     for (int x = 0; x < GAME_WIDTH; x++) {
@@ -15,63 +12,66 @@ void update_columns(pixel_t pixels[GAME_WIDTH][GAME_HEIGHT]) {
 
         for (int y = 0; y < GAME_HEIGHT; y++) { // there is no way this will ever be fast ever
 
-            pixel_t* pixel = &/* old_ */pixels[x][y];
+            pixel_t* pixel = &pixels[x][y];
+
+            if (pixel->color == NONE) {
+                below_is_empty = true;
+                continue;
+            }
             
             int x_change = 0;
             int y_change = 0;
+            
+            if (below_is_empty)
+                y_change = -1;
 
-            if (pixel->color != NONE) {
-                if (below_is_empty)
-                    y_change = -1;
+            // if the one below to the side is free then the one above it should also be free (hopefully)
+            // but like maybe not if it hasnt updated it yet (to the right) not anymore hahaha
+            else if (can_move_side(fall_direction) && pixels[x + fall_direction][y - 1].color == NONE && pixels[x + fall_direction][y].color == NONE) {
+                x_change = fall_direction;
+                y_change = -1;
 
-                // if the one below to the side is free then the one above it should also be free (hopefully)
-                // but like maybe not if it hasnt updated it yet (to the right) not anymore hahaha
-                else if (can_move_side(fall_direction) && pixels[x + fall_direction][y - 1].color == NONE && pixels[x + fall_direction][y].color == NONE) {
-                    x_change = fall_direction;
-                    y_change = -1;
+            } else if (can_move_side(-fall_direction) && pixels[x - fall_direction][y - 1].color == NONE && pixels[x - fall_direction][y].color == NONE) {
+                x_change = -fall_direction;
+                y_change = -1;
 
-                } else if (can_move_side(-fall_direction) && pixels[x - fall_direction][y - 1].color == NONE && pixels[x - fall_direction][y].color == NONE) {
-                    x_change = -fall_direction;
-                    y_change = -1;
-
-                }
             }
-
+            
             
             if (!pixel->has_moved && (x_change != 0 || y_change != 0)) {
-                /* pixel_t* new_pixel = &pixels[x][y]; */
-
-                dbg_printf("Moved from (%i, %i) to (%i, %i)\n", x, y, x + x_change, y + y_change);
 
                 pixels[x + x_change][y + y_change].color = pixel->color;
                 pixels[x + x_change][y + y_change].has_moved = true;
-                /* new_ */pixel->color = NONE;
-            }
+                pixel->color = NONE;
+                below_is_empty = true;
+
+            } else
+                below_is_empty = false;
 
             pixel->has_moved = false;
                 
 
-            below_is_empty = pixels[x][y].color == NONE;
+            // below_is_empty = pixels[x][y].color == NONE;
 
             // now maybe check for l/r connection idk how tf im sposta do that
-            if (pixel->color == NONE) continue;
+            // if (pixel->color == NONE) continue;
             
-            if (x == 0) {
-                pixel->is_connected_left = true;
-                continue;
-            }
+            // if (x == 0) {
+            //     pixel->is_connected_left = true;
+            //     continue;
+            // }
 
-            // c u u // how the hell do i determine if the bottom center one is connect left
-            // c u u // if i dont know that the one above is connected left
-            // x c u // maybe i just ignore it and hope in never happens (it will happens)
+            // // c u u // how the hell do i determine if the bottom center one is connect left
+            // // c u u // if i dont know that the one above is connected left
+            // // x c u // maybe i just ignore it and hope in never happens (it will happens)
             
-            pixel->is_connected_left =
-                (pixels[x - 1][y].is_connected_left && pixels[x - 1][y].color == pixel->color) ||
-                (pixels[x][y - 1].is_connected_left && pixels[x][y - 1].color == pixel->color) /*|| 
-                (pixels[x][y + 1].is_connected_left && pixels[x][y + 1].t == pixel->t)*/;
+            // pixel->is_connected_left =
+            //     (pixels[x - 1][y].is_connected_left && pixels[x - 1][y].color == pixel->color) ||
+            //     (pixels[x][y - 1].is_connected_left && pixels[x][y - 1].color == pixel->color) /*|| 
+            //     (pixels[x][y + 1].is_connected_left && pixels[x][y + 1].t == pixel->t)*/;
 
-            if (pixel->is_connected_left && x >= GAME_WIDTH - 1)
-                clear_from(x, y, pixel->color, pixels);
+            // if (pixel->is_connected_left && x >= GAME_WIDTH - 1)
+            //     clear_from(x, y, pixel->color, pixels);
             
         }
     }
